9ee4ea638d2569e12273706d40070777944fcf9a
Syed usman <Syed.usman@obsidian.com>
2024-09-05 23:55:50 -0700

    updated

---
+++ src/components/Tempapp.js
@@ -0,0 +1,35 @@
+import React   from "react";
+import "./css/style.css";
+const Tempapp  = () =>
+{
+return(
+    <>
+<div className="box">
+
+<div className="inputData">
+
+
+<input
+   type="search"
+   placeholder="Search here"
+   className="inputFeild"
+   onChange={(event) => {
+
+   }}
+   />
+
+</div>
+</div>
+
+<div className="info">
+<h2 className="location">
+usman
+</h2>
+
+</div>
+
+</>
+)
+}
+
+export default Tempapp;
